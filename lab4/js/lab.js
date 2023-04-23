fbc599eaeecda58c0b193def0050822b9bb4219d
ec41206 <78451212+Chaktis@users.noreply.github.com>
2023-04-22 17:05:16 -0700

    Created Lab 4 webpage and linked to main site

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,13 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Pseudocode for a hangman game
+// Author: Erin Casey
+// Date: 4/22/2023
 
-// Constants
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Choose a word from a list of words
+// Start the game
+    // Print underscores for how long the chosen word is
+    // Get input from user
+    // Check if letter is in the word
+      // If no, draw hangman part
+      // If yes, replace an underscore with the letter
+    // Repeat until hangman figure is complete or word has been guessed, then end the game
