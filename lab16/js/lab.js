39f0aa658d094aa8cd4061e918e5365355923ca9
ec41206 <78451212+Chaktis@users.noreply.github.com>
2023-06-05 12:13:57 -0700

    Finished lab 16 and linked to main page

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,21 +1,18 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Author: Erin Casey
+// Date: 6/5/2023
+function comicObj () {
+  $.ajax({
+    url: "https://xkcd.com/info.0.json",
+    data:{},
+    type: "GET",
+    dataType: "json",
+    success: function(data){
+      $("#output").html("<h3>" + data.title + "</h3>");
+      $("#output").append("<img src=" + data.img + ">");
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log("Error:", textStatus, errorThrown);
+    }
+  })
 }
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+$("#activate").click(comicObj);
